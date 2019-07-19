70a89506f2992408dbcf37caf74aeb11817d48c2
Hennes Römmer <hroemmer@googlemail.com>
2019-07-19 13:46:32 +0200

    FEAT: Add docs.zeit.de boilerplate files

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+project = 'Frontend Developer Handbook'
+copyright = 'ZEIT ONLINE'
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+html_theme = "sphinx_zon_theme"
+html_theme_options = {
+    'editme_link': (
+        'https://github.com/ZeitOnline/frontend-developer-handbook/edit/master/docs/{page}')
+}
+html_last_updated_fmt = '%b %d, %Y'
